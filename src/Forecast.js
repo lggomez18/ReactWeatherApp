b54f0483270efd86a54e7009b7ddb4de02c04a9e
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-05-28 21:53:13 +0000

    Adding today's temperature under forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -10,23 +10,36 @@ export default function Forecast() {
       <div className="containerF">
         <div className="row">
           <div className="column">
-            Hora:
+            <h2 id="date">
+              <small>Hora:</small>
+            </h2>
             <br />
             2:45 PM
             <br />
             <h2 id="date">
-              <small>Fecha</small>
+              <small>Fecha:</small>
             </h2>
             <br />
             01/01/2001
           </div>
 
           <div className="column">
-            <strong>Today</strong>
+            <h2>
+              <strong>Today</strong>
+            </h2>
             <br />
             Humidity: 80%
             <br />
             Wind Speed: 12 km/hr
+            <div class="temperature">
+              <a href="#" id="celsius-link" class="active">
+                21°C
+              </a>
+              |
+              <a href="#" id="fahrenheit-link">
+                70°F
+              </a>
+            </div>
           </div>
         </div>
       </div>
