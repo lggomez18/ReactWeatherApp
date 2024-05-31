f0035a66f370e74972a5605f86c8433940094d39
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-05-31 20:16:54 +0000

    Challenge 2, now we can see both temperatures when clicked on F.

--- src2/city.js
+++ src2/city.js
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 
 export default function City(props) {
-
-  const [temperature, setTemperature] = useState {props.temperature};
+  const [temperature, setTemperature] = useState(props.temperature);
 
   function Fahrenheit(event) {
     event.preventDefault();
 
     let fahrenheitTemperature = Math.round(props.temperature * (9 / 5) + 32);
-    alert(`${fahrenheitTemperature}°F`);
+    setTemperature(fahrenheitTemperature);
+    //alert(`${fahrenheitTemperature}°F`);
   }
 
   return (
     <div class="City">
-      It is {props.temperature}°C |{" "}
+      It is {temperature}°C |{" "}
       <a href="/" onClick={Fahrenheit}>
         °F{" "}
       </a>{" "}
