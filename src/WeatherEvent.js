81d994038acdc777331cf2545e41be44972e366f
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-05-31 17:38:44 +0000

    Adding an explanation why we are not using parenthesis right next to handleClick on the button element.

--- src/WeatherEvent.js
+++ src/WeatherEvent.js
@@ -7,3 +7,7 @@ export default function Weather() {
 
   return <button onClick={handleClick}>Current Temperature</button>;
 }
+
+//Add an event to the button, when clicked the function handleClick the button will display a message
+//In contrast to vainilla js, in react we add an event handler to a component using camel case naming convention like this -> {event name} with the event name OnClick before the event name -->onClick{eventname}
+//The name of the function is executed as the value of the event handler (in this case handleClick), without including the parentheses or the event object.
