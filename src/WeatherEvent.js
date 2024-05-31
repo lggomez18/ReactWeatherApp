a26cad2afc4643db11cbe7723fd497f95491eff1
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-05-31 17:25:24 +0000

    Buttion with a function in progress.

--- src/WeatherEvent.js
+++ src/WeatherEvent.js
@@ -1,11 +1,9 @@
 import React from "react";
 
 export default function Weather() {
-  //function temperature (event){
+  function handleClick (event){
   //event.preventDefault();
   //alert("Coming soon");
 
-  return <button>Current Temperature</button>;
+  return <button onClick={handleClick}>Current Temperature</button>;
 }
-
-//onClick={temperature}
