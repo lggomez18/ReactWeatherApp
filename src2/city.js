b09334f81bcd6bc160920c8f0fe98a895769d859
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-05-31 19:56:14 +0000

    Fahrenheit function in progress. Testing. Effective, when the F link is clicked, the temperature determined pops in a message.

--- src2/city.js
+++ src2/city.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 export default function City(props) {
+  function Fahrenheit(event) {
+    event.preventDefault();
+    alert(props.temperature);
+  }
+
   return (
     <div class="City">
       It is {props.temperature}°C |{" "}
-      <a href="/" onClick={showFarenheit}>
-        {" "}
+      <a href="/" onClick={Fahrenheit}>
         °F{" "}
       </a>{" "}
       in {props.name}
