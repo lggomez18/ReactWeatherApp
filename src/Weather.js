b3fb17ed417d6d36d1378b15c3dee31c53329983
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-05-31 01:40:16 +0000

    Learning how to create an event in react using functions

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 function Weather(props) {
-  //function farenheitTemperature() {
+  //function fahrenheitTemperature() {
   //let temperature = (props.temperature * 9) / 5 + 32;
   //return Math.round(temperature);
 
+  function showFahrenheit(event) {}
+  //as a result of using onClick, you must create en event to make the function showFahrenheit work.
   return (
     <div>
       The temperature in {props.city} is {props.temperature}°C |{" "}
-      <a href="/">
+      <a href="/" onClick={showFahrenheit}>
         {" "}
         {""}
         {Math.round(props.temperature * (9 / 5) + 32)}°F{" "}
@@ -16,5 +18,6 @@ function Weather(props) {
     </div>
   );
 }
-
+//onClick is used to for when the person clicks the link it automatically converts to fahrenheit.
+//onClick serves as an event listener
 export default Weather;
