b57d825e00e38d86a98e963949e6ce2578de0a65
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-05-31 17:27:56 +0000

    When the button is clicked, then a message is displayed.

--- src/WeatherEvent.js
+++ src/WeatherEvent.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 export default function Weather() {
-  function handleClick (event){
-  //event.preventDefault();
-  //alert("Coming soon");
+  function handleClick() {
+    alert("Coming soon");
+  }
 
   return <button onClick={handleClick}>Current Temperature</button>;
 }
