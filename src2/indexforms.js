e042ad3128fa3e48a00913e48c4389802e709a27
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-06-05 15:59:37 +0000

    Created a component called index forms to learn how to develop forms using react

---
+++ src2/indexforms.js
@@ -0,0 +1,32 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./App.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <div className="App">
+      <div>
+        <h1>React Forms</h1>
+
+        <h3>Challenge 1</h3>
+        <p>
+          Create a new component called Search with a search input and a button.
+          Include it here.
+        </p>
+
+        <h3>Challenge 2</h3>
+        <p>When the form is submitted, alert "Searching.." Include it here.</p>
+
+        <h3>Challenge 3</h3>
+        <p>
+          When the form is submitted with New York in the search input for
+          example, alert "New York" Include it here.
+        </p>
+      </div>
+    </div>
+  </StrictMode>,
+);
