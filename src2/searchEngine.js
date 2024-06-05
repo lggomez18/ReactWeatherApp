92be83077c57f6819212d45a4c3b564d97d85105
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-06-05 16:45:44 +0000

    Forms in process. Testing accessibility features.

--- src2/searchEngine.js
+++ src2/searchEngine.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function searchEngine() {
+export default function Search() {
   return (
     <form>
-      <input type="search" />
+      <input type="search" id="search" name="search" aria-label="Search" />
       <input type="button" value="search" />
     </form>
   );
