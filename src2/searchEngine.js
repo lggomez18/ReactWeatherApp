27a57c625b4a7341b41081492d6f5d3ef461e25c
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-06-05 16:49:21 +0000

    When clicking submit, forms alerts a message. Issue debugged. WACG guidelines for using appropriate labels in progress.

--- src2/searchEngine.js
+++ src2/searchEngine.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 export default function Search() {
+  function handleSearch(event) {
+    event.preventDefault();
+    alert("Searching...");
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSearch}>
       <input type="search" id="search" name="search" aria-label="Search" />
       <input type="button" value="search" />
     </form>
