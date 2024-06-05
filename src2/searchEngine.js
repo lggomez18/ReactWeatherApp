02f7e8677e787e30a16dfdefddfe8ce792bfc795
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-06-05 17:02:31 +0000

    Search function in progress. Debugging issues.

--- src2/searchEngine.js
+++ src2/searchEngine.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Search() {
+  const [query, setQuery] = useState("");
+
   function handleSearch(event) {
     event.preventDefault();
     alert("Searching...");
   }
 
+  function updateQuery(event) {}
+
   return (
     <form onSubmit={handleSearch}>
-      <input type="search" id="search" name="search" aria-label="Search" />
+      <strong>The query is:{query} </strong>
+      <br />
+      <input type="search" onChange={updateQuery} />
       <input type="button" value="search" />
     </form>
   );
