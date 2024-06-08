c3f25ea6a55e6aab97990d285096d6f51de648d5
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-06-08 15:21:13 +0000

    Added form element

--- src/Search.js
+++ src/Search.js
@@ -1,3 +1,10 @@
 import React from "react";
 
-export default function SearchEngine() {}
+export default function SearchEngine() {
+  return (
+    <form>
+      <input type="search"></input>
+      <input type="submit"></input>
+    </form>
+  );
+}
