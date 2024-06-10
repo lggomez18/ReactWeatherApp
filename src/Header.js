2ba2fa58e61b1c9769d7c75cf168e99ed2e8a367
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-06-10 21:37:54 +0000

    Debugging search bar

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,6 @@
 //1. Import react. REMEMBER to type the word React with capital letter on the first one, and all lowercase on the second-react.
 import React from "react";
 import Search from "./Search";
-import Forecast from "./Forecast";
-import Weather from "./Weather";
-import City from "./city";
 import Footer from "./Footer";
 
 //2. Export the function
